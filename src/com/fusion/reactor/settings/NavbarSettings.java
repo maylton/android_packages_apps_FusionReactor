TeamTwisted/packages_apps_Settings

Longpress back to kill app [2/2]
Part two of https://github.com/zephiK/android_frameworks_base/commit/132a7a7e80beaf3ffa4c6428b30c2349569ed8d0
Originating commit from rascarlo - https://github.com/rascarlo/android_frameworks_base/commit/cc7cbd4c359bb88d0f482ca19b1c2285643cbe71

Change-Id: I7693f33a6e4dc7d04a12a1051e5b7cbd65b8203d

src/com/android/settings/benzo/NavbarSettings.java
@@ -16,11 +16,22 @@
 package com.android.settings.benzo;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
 
-public class NavbarSettings extends SettingsPreferenceFragment {
+public class NavbarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
+    private SwitchPreference mKillAppLongPressBack;
 
     @Override
     protected int getMetricsCategory() {
@@ -31,5 +42,22 @@ public class NavbarSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.navbar_settings);
+
+        // kill-app long press back
+        mKillAppLongPressBack = (SwitchPreference) findPreference(KILL_APP_LONGPRESS_BACK);
+        mKillAppLongPressBack.setOnPreferenceChangeListener(this);
+        int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
+                KILL_APP_LONGPRESS_BACK, 0);
+        mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mKillAppLongPressBack) {
+            boolean value = (Boolean) objValue;
+            Settings.Secure.putInt(getContentResolver(), KILL_APP_LONGPRESS_BACK,
+                    value ? 1 : 0);
+        }
+            return true;
     }
 }

maylton/android_packages_apps_FusionReactor

Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: Eliminater74 <eliminater74@gmail.com>

src/com/fusion/reactor/settings/NavbarSettings.java
@@ -53,6 +53,8 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
     private static final String PREF_BATT_BAR_WIDTH = "battery_bar_thickness";
     private static final String PREF_BATT_ANIMATE = "battery_bar_animate";
 
+    private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+
     private ListPreference mBatteryBar;
     private ListPreference mBatteryBarStyle;
     private ListPreference mBatteryBarThickness;
@@ -63,6 +65,8 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
     private ColorPickerPreference mBatteryBarBatteryLowColor;
     private ColorPickerPreference mBatteryBarBatteryLowColorWarn;
     private ColorPickerPreference mBatteryBarBatteryHighColor;
+
+    private SwitchPreference mKillAppLongPressBack;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -146,6 +150,13 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
                 Settings.System.BATTERY_BAR_THICKNESS, 1)) + "");
         mBatteryBarThickness.setSummary(mBatteryBarThickness.getEntry());
 
+        // kill-app long press back
+        mKillAppLongPressBack = (SwitchPreference) findPreference(KILL_APP_LONGPRESS_BACK);
+        mKillAppLongPressBack.setOnPreferenceChangeListener(this);
+        int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
+                KILL_APP_LONGPRESS_BACK, 0);
+        mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
+
         updateBatteryBarOptions();
     }
 
@@ -207,6 +218,11 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
             int val = Integer.parseInt((String) newValue);
             return Settings.System.putInt(resolver,
                     Settings.System.BATTERY_BAR_THICKNESS, val);
+        } else if (preference == mKillAppLongPressBack) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+		KILL_APP_LONGPRESS_BACK, value ? 1 : 0);
+            return true;
        }
        return false;
      }